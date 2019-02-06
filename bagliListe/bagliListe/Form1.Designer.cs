1912c4e153e28b8dac70c1d403de6b70e8566abc
unknown <be_siktas_1903@hotmail.com>
2019-02-06 09:21:19 +0300

    gonderme

---
+++ bagliListe/bagliListe/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace bagliListe
+{
+    unsafe partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+         protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnGroup = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(251, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sayıları Arada Virgül Olacak Şekilde Giriniz:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(579, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Bağlı Liste Oluştur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(21, 95);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(165, 318);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Değer";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Adres(Sonraki)";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 100;
+            // 
+            // btnGroup
+            // 
+            this.btnGroup.Location = new System.Drawing.Point(215, 95);
+            this.btnGroup.Name = "btnGroup";
+            this.btnGroup.Size = new System.Drawing.Size(563, 318);
+            this.btnGroup.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(124, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGroup);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel btnGroup;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bagliListe/bagliListe/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace bagliListe
+{
+    public unsafe partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        public class BListe
+        {
+            public int data;
+            public int* sonraki;
+
+        }
+
+       int say3 = 0,yks=25,yer=0;
+        ListViewItem lst;
+        int var2 = 0;
+       
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                for (int lcount = 0; lcount <= listView1.Items.Count - 1; lcount++)
+                {
+                    if (listView1.Items[lcount].Selected == true)
+                    {
+                        var2 = lcount;
+                        break;
+                    }
+                }
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (var2 == (listView1.Items.Count-1))
+                {
+                    listView1.SelectedItems[0].Remove();
+                    listView1.Items[listView1.Items.Count - 1].SubItems[1].Text = "000000000";
+                }
+                else
+                {
+                    listView1.SelectedItems[0].Remove();
+                }
+
+            
+            btnGroup.Controls.Clear();
+            yer = 0;
+            yks = 25;
+            int n = 1, m = 1;
+
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+            {
+           
+            int countB = 0;
+                foreach (Control c in btnGroup.Controls)
+                {
+                    if (c.GetType() == typeof(Button))
+                    {
+                        countB += 1;
+
+                    }
+                }
+
+                Button btn = new Button();
+            
+            if (countB != 0 && countB % 6 == 0)
+            {
+                yks += 50;
+                yer = 0;
+                btn.Location = new Point(yer, yks);
+                yer = 150;
+            }
+            else
+            {
+                btn.Location = new Point(yer, yks);
+
+
+            }
+                    if (n % 2 == 1)
+                    {
+                        n += 1;
+                        btn.BackColor = Color.LawnGreen;
+                    }
+                    else
+                    {
+                        n += 1;
+                        btn.BackColor = Color.Yellow;
+                    }
+
+                    btn.Name = "btn_" + i.ToString();
+            btn.Size = new Size(50, 30);
+            btn.Text = listView1.Items[i].SubItems[0].Text;
+            btnGroup.Controls.Add(btn);
+
+            Button btn2 = new Button();
+            if (countB != 0 && countB % 6 == 0)
+            {
+
+                btn2.Location = new Point(40, yks);
+            }
+            else
+            {
+
+                btn2.Location = new Point(40 + yer, yks);
+                yer += 150;
+            }
+
+                    if (m % 2 == 1)
+                    {
+                        m += 1;
+                        btn2.BackColor = Color.Yellow;
+                    }
+                    else
+                    {
+                        m += 1;
+                        btn2.BackColor = Color.LawnGreen;
+                    }
+                    btn2.Name = "btn2_" + i.ToString();
+            btn2.Size = new Size(90, 30);
+            btn2.Text = listView1.Items[i].SubItems[1].Text; ;
+            btnGroup.Controls.Add(btn2);
+            }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Lütfen önce silinecek veriyi seçiniz");
+            }
+        }
+
+
+        private unsafe void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            say3 = 0;
+            listView1.Items.Clear();
+            btnGroup.Controls.Clear();
+            int sayi = 0, sayi2 = 0;
+            BListe p1 = new BListe();
+            BListe p2 = new BListe();
+            yer = 0;
+            yks = 25;
+            int n = 1,m = 1;
+
+            foreach (var item in textBox1.Text.Split(','))
+            {
+                sayi2 +=1;
+            }
+
+            int[] dizi = new int[sayi2];
+
+            foreach (var item in textBox1.Text.Split(','))
+            {
+
+                int deger = Convert.ToInt32(item);
+                dizi[sayi] = deger;
+                sayi += 1;
+            }
+
+
+           
+            for (int i = 0; i < dizi.Length; i++) {
+
+                say3 += 1;
+                sayi = Convert.ToInt32(dizi[i]);
+                p1.data = sayi;
+               
+                int countB = 0;
+                foreach (Control c in btnGroup.Controls)
+                {
+                    if (c.GetType() == typeof(Button))
+                    {
+                        countB += 1;
+
+                    }
+                }
+
+
+                if (i != dizi.Length - 1)
+                    {
+                        sayi2 = Convert.ToInt32(dizi[i + 1]);
+                        p2.data = sayi2;
+                        p2.sonraki = &sayi2;
+                    p1.sonraki = p2.sonraki;
+                       
+                }
+                    else
+                    {
+                        p2.sonraki=null;
+                        p1.sonraki = p2.sonraki;
+                }
+
+                if (p2.sonraki == null) {
+                    string[] bilgiler = { p1.data.ToString(), "000000000" };
+                    lst = new ListViewItem(bilgiler);
+                    listView1.Items.Add(lst);
+                }
+                else
+                {
+                    string[] bilgiler = { p1.data.ToString(), ((int)p1.sonraki).ToString() + say3 };
+                     lst = new ListViewItem(bilgiler);
+                    listView1.Items.Add(lst);
+                }
+               
+                     
+
+                    Button btn = new Button();
+                if (countB!=0 && countB % 6 == 0)
+                {
+                    yks += 50;
+                    yer = 0;
+                    btn.Location = new Point(yer,yks);
+                    yer = 150;
+                }
+                else
+                {
+                    btn.Location = new Point(yer, yks);
+                  
+                   
+                }
+                    if (n%2==1)
+                    {
+                        n += 1;
+                        btn.BackColor = Color.LawnGreen;
+                    }
+                    else
+                    {
+                        n += 1;
+                        btn.BackColor = Color.Yellow;
+                    }
+                    btn.Name = "btn_" + i.ToString();
+                    btn.Size = new Size(50, 30);
+                    btn.Text = lst.SubItems[0].Text;
+                    btnGroup.Controls.Add(btn);
+               
+                    Button btn2 = new Button();
+                if (countB != 0 && countB % 6 == 0)
+                {
+                   
+                    btn2.Location = new Point(40, yks);
+                }
+                else
+                {
+                    
+                    btn2.Location = new Point(40 + yer, yks);
+                    yer += 150;
+                }
+
+                    if (m%2==1)
+                    {
+                        m += 1;
+                        btn2.BackColor = Color.Yellow;
+                    }
+                    else
+                    {
+                        m += 1;
+                        btn2.BackColor = Color.LawnGreen;
+                    }
+                    btn2.Name = "btn2_" + i.ToString();
+                    btn2.Size = new Size(90, 30);
+                    btn2.Text = lst.SubItems[1].Text; ;
+                    btnGroup.Controls.Add(btn2);
+
+               
+
+            }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Bağlantılı Liste Oluşturulamadı...");
+            }
+
+
+
+        }
+    }
+}
